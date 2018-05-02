2f28f928ea0282d024e94d8806895f13f503f70e
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-05-02 15:34:01 +0900

    add

--- Simple TableView/ViewController.swift
+++ Simple TableView/ViewController.swift
@@ -1,49 +1,64 @@
-//
-//  ViewController.swift
-//  Simple TableView
-//
-//  Created by 김종현 on 2018. 4. 30..
-//  Copyright © 2018년 김종현. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var myTableView: UITableView!
-    var animals = ["Cow", "Pig", "Dog", "Rabbit", "Bird"]
-    var year = ["3", "5", "2", "4", "5"]
-    
+    var animals = ["Cat", "Dog", "Cow", "Rabbit", "Bird"]
+    var year = ["고양이", "강아지", "소", "토끼", "새"]
+    var img = ["1.jpg","2.jpg","3.jpg"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // UIViewController와 Delegate 연결
+        
         myTableView.delegate = self
         myTableView.dataSource = self
     
     }
     
-    // UITableViewDataSoure
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return animals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let identifier = "Cell"
+        let identifier = "RE"
         let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         
-        // cell에 image 넣기
-        let myImage = UIImage(named:"cat.png")
+        
+        let myImage = UIImage(named:"1.jpg")
         cell.imageView?.image = myImage
-        
-        // cell에 text 넣기
         cell.textLabel?.text = animals[indexPath.row]
-        
-        // cell에 detailText 넣기
         cell.detailTextLabel?.text = year[indexPath.row]
         
         return cell
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("section = \(indexPath.section) row = \(indexPath.row)")
+        print(animals[indexPath.row])
+        print(year[indexPath.row])
+        
+        let myAlert = UIAlertController(title: "동물들",message: "동물에대해 알아보자",preferredStyle: .actionSheet)
+        
+        let ok = UIAlertAction(title: "확인", style: .default) { (action: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.red
+        }
+        
+        let cancelAction = UIAlertAction(title: "삭제", style: .cancel, handler: { (action: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.green
+        })
+        
+        let testAction = UIAlertAction(title: "취소", style: .default, handler:  nil)
+        
+        myAlert.addAction(ok)
+        myAlert.addAction(cancelAction)
+        myAlert.addAction(testAction)
+        
+        present(myAlert, animated: true, completion: nil)
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return  100
+    }
+    }
 
-}
 
